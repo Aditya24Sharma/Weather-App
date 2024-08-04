fd292c98fc909bd7fed8b2b4a7ac551346ad7fe7
Aditya Sharma <aditya2002.05.19@gmail.com>
2024-08-03 22:51:45 -0400

    hourly forecast function

--- weather-app-backend/app.py
+++ weather-app-backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify 
 import requests
-from getWeather import get_current_weather
+from getWeather import get_weather_response, hourly_forecast_df
 
 #creating an instance of the Flask class '__name__' so that Flask knows where to look for resources such as template and static files
 app = Flask(__name__)
@@ -15,16 +15,32 @@ app = Flask(__name__)
 # } 
 @app.route('/')
 def home():
-    currentWeather = get_current_weather()
-    currentTemperature = currentWeather.Variables(0).Value()
-    return f'<p>The current weather is {currentTemperature:.2f}</p>'
+    response = get_weather_response(42.4,-71.05)
+    current = response.Current()
+    current_temperature_2m = current.Variables(0).Value()
+    current_apparent_temperature = current.Variables(1).Value()
+    current_is_day = current.Variables(2).Value()
+    current_precipitation = current.Variables(3).Value()
+    current_weather_code = current.Variables(4).Value()
 
-@app.route('/weather', methods = ['GET'])
-def get_weather():
-    link = 'https://open-meteo.com/en/docs#latitude=42.3584&longitude=-71.0598&current=temperature_2m,apparent_temperature,is_day,precipitation,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_sum&temperature_unit=fahrenheit'
-    response = requests.get(link)
-    print(response.content)
+    print(f"Current time {current.Time()}")
+    print(f"Current temperature_2m {current_temperature_2m}")
+    print(f"Current apparent_temperature {current_apparent_temperature}")
+    print(f"Current is_day {current_is_day}")
+    print(f"Current precipitation {current_precipitation}")
+    print(f"Current weather_code {current_weather_code}")
 
+    return f'<p>The current weather is {current_temperature_2m:.2f}</p>'
+
+@app.route('/hourly')
+def hourly_weather():
+    response = get_weather_response(42.4,-71.05)
+    hourly_forecast = hourly_forecast_df(response)
+    output = '<p>'
+    for i in range(1, 5):
+        output += f"<p>{hourly_forecast['date'][i]} -> {hourly_forecast['temperature_2m'][i]}" + '</p>' 
+
+    return output
 
 if __name__== '__main__':
     app.run(debug = True)
--- weather-app-backend/getWeather.py
+++ weather-app-backend/getWeather.py
@@ -3,6 +3,8 @@ from openmeteo_sdk.Variable import Variable
 
 import requests_cache
 import pandas as pd
+import numpy as np
+
 from retry_requests import retry
 
 def cache_weather():
@@ -11,7 +13,7 @@ def cache_weather():
     retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
     return retry_session
 
-def get_current_weather(latitude = 52.54, longitude = 13.41):
+def get_weather_response(latitude = 52.54, longitude = 13.41):
     session = cache_weather()
     openmeteo = openmeteo_requests.Client(session = session)
     url = "https://api.open-meteo.com/v1/forecast"
@@ -27,8 +29,30 @@ def get_current_weather(latitude = 52.54, longitude = 13.41):
 
     # just processing the first location as response might give multiple locations
     response = responses[0]
-    current = response.Current()
-    return current
+    return response
+
+def hourly_forecast_df(response):
+    hourly = response.Hourly()
+    hourly_time = range(hourly.Time(), hourly.TimeEnd(), hourly.Interval())
+    hourly_variables = list(map(lambda i: hourly.Variables(i), range(0, hourly.VariablesLength())))
+
+    hourly_temperature_2m = next(filter(lambda x: x.Variable() == Variable.temperature and x.Altitude() == 2, hourly_variables)).ValuesAsNumpy()
+
+    hourly_data = {"date": pd.date_range(
+        start = pd.to_datetime(hourly.Time(), unit = "s"),
+        end = pd.to_datetime(hourly.TimeEnd(), unit = "s"),
+        freq = pd.Timedelta(seconds = hourly.Interval()),
+        inclusive = "left"
+    )}
+    hourly_data["temperature_2m"] = hourly_temperature_2m
+
+    hourly_dataframe_pd = pd.DataFrame(data = hourly_data)
+    for i in range(1, 5):
+        print(f"{hourly_dataframe_pd['date'][i]} -> {hourly_dataframe_pd['temperature_2m'][i]}")
+        print()
+
+    return hourly_dataframe_pd
 
 if __name__ == '__main__':
-    get_current_weather()
+    response = get_weather_response(42.4,-71.05)
+    hourly_forecast_df(response)
