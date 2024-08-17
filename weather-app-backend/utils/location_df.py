20d1be9fac07c71b833be582c6613489c0cbbd2c
Aditya Sharma <aditya2002.05.19@gmail.com>
2024-08-16 22:12:02 -0400

    path fixed

--- weather-app-backend/utils/location_df.py
+++ weather-app-backend/utils/location_df.py
@@ -1,21 +1,24 @@
 import pandas as pd
 import duckdb
+import os
 
 #Description: Take zip code and returns info about that location after querying through the dataset
 #return : dictionary (zip,type,primary_city,state,county,standard_timezone,place_timezone,area_codes,latitude,longitude)
 
 def get_location_info(zip_code = 39406):
-    location_df = duckdb.sql(f"""
-        SELECT * FROM '../../location_dataset/Zip_codes_with_location.csv' as 'dataset'
-        WHERE zip = {zip_code}
-            """
-    ).df()
+    file_path = 'Zip_codes_with_location.csv'
+    abs_file_path = os.path.normpath(file_path)
+    query = f"""
+    SELECT * FROM '{abs_file_path}' as 'dataset'   
+    WHERE zip = {zip_code}
+        """
+    location_df = duckdb.sql(query).df()
 
     location_dict = location_df.to_dict('list')
     for key, values in location_dict.items():
         location_dict[key] = location_dict[key][0]
 
-    # print(type(location_dict['zip']))
+    # print(location_dict['county'])
     return location_dict
 
 if __name__ == '__main__':
--- weather-app-frontend/src/App.js
+++ weather-app-frontend/src/App.js
@@ -1,12 +1,18 @@
 import React, {useState, useEffect} from 'react'
 
-
-
-
-function App() {
-
+function App(){
   const [currentTemp, setTemp] = useState(0);
   const [currentTime, setTime] = useState('')
+  const [currentZipCode, setZipCode] = useState(0);
+
+
+
+  function backend_fetch(){
+      fetch('/home')
+      .then(res => res.json())
+      .then(data => {setTemp(data.current_temp); convert_24hr(data.time);});
+  }
+    
 
   function convert_24hr(timestamp){
     const date = new Date(timestamp*1000);
@@ -20,12 +26,19 @@ function App() {
   }
 
   useEffect(()=>{
-    fetch('/home')
-    .then(res => res.json())
-    .then(data => {setTemp(data.current_temp); convert_24hr(data.time);});
+   backend_fetch();
   }, [])
+
+  const handleInputChange = (event) => {
+    setZipCode(event.target.value);
+  };
+
+
   return (
     <div>
+      <input type='text' placeholder='hello'  onChange={handleInputChange} />
+      <button type='submit' onClick={backend_fetch(currentZipCode)}>Click me</button>
+      {currentZipCode}
       <p>The current Temp is {currentTemp}</p>
       <p>The current Time is {currentTime}</p>
     </div>
